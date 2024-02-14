420e0664cfb0821c08bf036b80e5627bf3a2989f
tigerwill90 <sylvainmuller@protonmail.com>
2024-02-14 13:17:00 +0100

    feat: do not rely on deprecated TeeWriter

---
+++ response_writer.go
@@ -0,0 +1,61 @@
+package foxdump
+
+import (
+	"bytes"
+	"github.com/tigerwill90/fox"
+	"io"
+	"net/http"
+)
+
+type multiWriter struct {
+	fox.ResponseWriter
+	buf *bytes.Buffer
+}
+
+func (w multiWriter) Unwrap() http.ResponseWriter {
+	return w.ResponseWriter
+}
+
+func (w multiWriter) Write(p []byte) (n int, err error) {
+	n, err = w.ResponseWriter.Write(p)
+	if err != nil {
+		return
+	}
+	if n != len(p) {
+		err = io.ErrShortWrite
+		return
+	}
+
+	n, err = w.buf.Write(p)
+	if err != nil {
+		return
+	}
+	if n != len(p) {
+		err = io.ErrShortWrite
+		return
+	}
+
+	return n, nil
+}
+
+func (w multiWriter) WriteString(s string) (n int, err error) {
+	n, err = w.ResponseWriter.WriteString(s)
+	if err != nil {
+		return
+	}
+	if n != len(s) {
+		err = io.ErrShortWrite
+		return
+	}
+
+	n, err = w.buf.WriteString(s)
+	if err != nil {
+		return
+	}
+	if n != len(s) {
+		err = io.ErrShortWrite
+		return
+	}
+
+	return n, nil
+}
